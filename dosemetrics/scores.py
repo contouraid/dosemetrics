eb40bb14cdb2b178aa522b4ed89562d8b50c8c69
AKA Math <amithjkamath@users.noreply.github.com>
2024-01-24 22:53:27 +0100

    More tests for scores. Fixed minor issues in DVH.

--- dosemetrics/scores.py
+++ dosemetrics/scores.py
@@ -1,11 +1,12 @@
 import numpy as np
+from numpy import ndarray
 
 """
 These are modified from https://github.com/ababier/open-kbp
 """
 
 
-def dose_score(_pred: np.ndarray, _gt: np.ndarray, _dose_mask=None):
+def dose_score(_pred: np.ndarray, _gt: np.ndarray, _dose_mask=None) -> np.ndarray:
     if _dose_mask is not None:
         _pred = _pred[_dose_mask > 0]
         _gt = _gt[_dose_mask > 0]
@@ -13,10 +14,12 @@ def dose_score(_pred: np.ndarray, _gt: np.ndarray, _dose_mask=None):
     return np.mean(np.abs(_pred - _gt))
 
 
-def dvh_score(_dose, _mask, mode, spacing=None):
+def dvh_score(
+    _dose: np.ndarray, _mask: np.ndarray, mode: str, spacing=None
+) -> dict[str, np.ndarray]:
     output = {}
 
-    if mode == "target":
+    if mode.lower() == "target":
         _roi_dose = _dose[_mask > 0]
         # D1
         output["D1"] = np.percentile(_roi_dose, 99)
@@ -25,7 +28,7 @@ def dvh_score(_dose, _mask, mode, spacing=None):
         # D99
         output["D99"] = np.percentile(_roi_dose, 1)
 
-    elif mode == "OAR":
+    elif mode.upper() == "OAR":
         if spacing is None:
             raise Exception("dvh score computation requires voxel spacing information.")
 
@@ -37,7 +40,7 @@ def dvh_score(_dose, _mask, mode, spacing=None):
         voxels_in_tenth_of_cc = np.maximum(1, np.round(100 / _voxel_size))
         fractional_volume_to_evaluate = 100 - voxels_in_tenth_of_cc / _roi_size * 100
         if fractional_volume_to_evaluate <= 0:
-            output["D_0.1_cc"] = 0.0
+            output["D_0.1_cc"] = np.asarray(0.0)
         else:
             output["D_0.1_cc"] = np.percentile(_roi_dose, fractional_volume_to_evaluate)
 
--- test/test_dvh.py
+++ test/test_dvh.py
@@ -5,9 +5,9 @@ import unittest
 
 class TestDVH(unittest.TestCase):
     def test_compute_dvh_single_voxel(self):
-        zero_dose = np.ndarray((10, 10, 10))
+        zero_dose = np.zeros((10, 10, 10))
         single_voxel_struct = np.zeros((10, 10, 10))
-        single_voxel_struct[5, 5, 5] = 1
+        single_voxel_struct[5, 5, 5] = 1.0
         results = dvh.compute_dvh(zero_dose, single_voxel_struct)
 
         self.assertTrue(len(results[0]) == 650)
@@ -23,8 +23,9 @@ class TestDVH(unittest.TestCase):
             self.assertTrue(results[1][i] == 0.0)
 
     def test_compute_dvh_zero_voxels(self):
-        zero_dose = np.ndarray((10, 10, 10))
+        zero_dose = np.zeros((10, 10, 10))
         single_voxel_struct = np.zeros((10, 10, 10))
+        single_voxel_struct[5, 5, 5] = 1.0
         max_dose = [10, 20, 30]
         step_size = [1, 2, 5]
 
@@ -44,17 +45,6 @@ class TestDVH(unittest.TestCase):
                 # Check that single voxel is indeed 0 dose.
                 for i in range(1, md // ss):
                     self.assertTrue(results[1][i] == 0.0)
-
-    def test_isupper(self):
-        self.assertTrue("FOO".isupper())
-        self.assertFalse("Foo".isupper())
-
-    def test_split(self):
-        s = "hello world"
-        self.assertEqual(s.split(), ["hello", "world"])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
 
 
 if __name__ == "__main__":
