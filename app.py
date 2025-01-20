782a1e81516dbfb5b3f4de5bc701c8a1d2379689
AKA Math <amithjkamath@users.noreply.github.com>
2025-01-20 15:32:04 +0100

    Testing out plotting on the server version.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import hmac
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import altair as alt
 import plotly.express as px
 from dosemetrics.dvh import dvh_from_files
-# Initial code from here: https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app
+from dosemetrics.plot import view_dose
+
 # Run this from >> streamlit run app.py
 
 
@@ -68,29 +71,11 @@ def main_loop():
     if not check_password():
         st.stop()
 
-    def instructions():
-        st.write("# DVH Calculator:")
-        st.sidebar.success("Select an option above.")
-
-        st.markdown(
-            """
-            This web-app calculates DVH curves and allied statistics from a Dose distribution + segmentation masks.
-    
-            This lives here: [contouraid-dvh-calculator.streamlit.app](https://contouraid-dvh-calculator.streamlit.app).
-    
-            ### Want to learn more?
-    
-            - Check out [www.contouraid.com](https://www.contouraid.com) for more info
-            - Reach out back to us at [www.contouraid.com/contact](https://www.contouraid.com/contact)
-        """
-        )
-
-
     def calculate_dvh():
         st.markdown(f"# {list(page_names_to_funcs.keys())[1]}")
 
         st.markdown(f"## Step 1: Upload dose distribution volume and mask files")
-        st.markdown(f"Look [here](https://pyradise.readthedocs.io) to format your files into NIfTI format using Pyradise.")
+        st.markdown(f"Check instructions on the sidebar for more information.")
         dose_file = st.file_uploader("Upload a dose distribution volume (in .nii.gz)", type=['nii', 'gz'])
 
         mask_files = st.file_uploader("Upload mask volumes (in .nii.gz)", accept_multiple_files=True, type=['nii', 'gz'])
@@ -111,10 +96,35 @@ def main_loop():
                 fig.update_yaxes(showgrid=True)
                 st.plotly_chart(fig, use_container_width=True)
 
+                # Create Figure beforehand
+                fig, ax = plt.subplots()
+                data = view_dose(dose_file, slice=50)
+                #arr = np.random.normal(1, 1, size=100)
+                #ax.hist(arr, bins=20)
+                plt.imshow(data, cmap="hot")
+                plt.title("Hello plotting!")
+                st.pyplot(fig)
+
+    def instructions():
+        st.write("# Dosemetrics Instructions:")
+        st.sidebar.success("Select an option above.")
+
+        st.markdown(
+            """
+            This web-app calculates statistics from a Dose distribution + segmentation masks.
+
+            This lives here: [dosemetrics.streamlit.app](https://dosemetrics.streamlit.app).
+
+            ### Want to learn more?
+
+            - Check out [www.contouraid.com](https://www.contouraid.com) for more information.
+        """
+        )
+
 
     page_names_to_funcs = {
-        "Instructions": instructions,
-        "Calculate DVH": calculate_dvh
+        "Calculate DVH": calculate_dvh,
+        "Instructions": instructions
     }
 
     task_selection = st.sidebar.selectbox("Choose a task:", page_names_to_funcs.keys())
