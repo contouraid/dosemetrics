1eac95cd2b459d62d694087682881a83388bc1a0
AKA Math <amithjkamath@users.noreply.github.com>
2025-01-21 13:11:44 +0100

    Adding gradient computation code.

--- examples/plot_all_dvh_per_case.py
+++ examples/plot_all_dvh_per_case.py
@@ -11,46 +11,51 @@ import dosemetrics.data_utils as data_utils
 def plot_dvh_to_pdf(data_folder, output_file="dvh.pdf"):
 
     pp = PdfPages(output_file)
-    contents_file = [f for f in os.listdir(data_folder) if f.endswith(".csv")][0]
-    contents = pd.read_csv(os.path.join(data_folder, contents_file))
+    contents_files = [f for f in os.listdir(data_folder) if f.endswith(".csv")]
+    if len(contents_files) > 0:
+        contents_file = contents_files[0]
+        contents = pd.read_csv(os.path.join(data_folder, contents_file))
 
-    dose_file = os.path.join(data_folder, "Dose.nii.gz")
-    dose_volume = data_utils.read_from_nifti(dose_file)
+        dose_file = os.path.join(data_folder, "Dose.nii.gz")
+        dose_volume = data_utils.read_from_nifti(dose_file)
 
-    structure_masks = {}
-    oar_list = contents[contents["Type"] == "OAR"]["Structure"].values
-    for oar in oar_list:
-        oar_file = os.path.join(data_folder, oar + ".nii.gz")
-        oar_volume = data_utils.read_from_nifti(oar_file)
-        structure_masks[oar] = oar_volume
+        structure_masks = {}
+        oar_list = contents[contents["Type"] == "OAR"]["Structure"].values
+        for oar in oar_list:
+            oar_file = os.path.join(data_folder, oar + ".nii.gz")
+            oar_volume = data_utils.read_from_nifti(oar_file)
+            structure_masks[oar] = oar_volume
 
-    target_list = contents[contents["Type"] == "Target"]["Structure"].values
-    for target in target_list:
-        target_file = os.path.join(data_folder, target + ".nii.gz")
-        target_volume = data_utils.read_from_nifti(target_file)
-        structure_masks[target] = target_volume
+        target_list = contents[contents["Type"] == "Target"]["Structure"].values
+        for target in target_list:
+            target_file = os.path.join(data_folder, target + ".nii.gz")
+            target_volume = data_utils.read_from_nifti(target_file)
+            structure_masks[target] = target_volume
 
-    fig, ax = plt.subplots()
-    fig.set_figheight(8)
-    fig.set_figwidth(12)
-    for structure in structure_masks.keys():
-        bins, values = dvh.compute_dvh(dose_volume, structure_masks[structure])
-        ax.plot(bins, values, label=structure)
+        fig, ax = plt.subplots()
+        fig.set_figheight(8)
+        fig.set_figwidth(12)
+        for structure in structure_masks.keys():
+            bins, values = dvh.compute_dvh(dose_volume, structure_masks[structure])
+            ax.plot(bins, values, label=structure)
 
-    plt.legend()
-    plt.grid()
-    ax.legend(loc="center left", bbox_to_anchor=(0.95, 0.5))
-    pp.savefig(fig, dpi=300)
-    pp.close()
+        plt.legend()
+        plt.grid()
+        ax.legend(loc="center left", bbox_to_anchor=(0.95, 0.5))
+        pp.savefig(fig, dpi=300)
+        pp.close()
 
 
 if __name__ == "__main__":
 
-    dataset_root = "/Users/amithkamath/data/USZ/usz-processed-v2025-2"
+    dataset_root = "/Users/amithkamath/data/Insel/training-data"
+    output_folder = os.path.join(dataset_root, "..", "quality-check")
+    os.makedirs(output_folder, exist_ok=True)
+
     subfolders = [f.path for f in os.scandir(dataset_root) if f.is_dir()]
     subfolders = sorted(subfolders)
 
     for subject_folder in subfolders:
         subject_name = subject_folder.split("/")[-1]
-        dvh_file = os.path.join(dataset_root, "..", subject_name + "_dvh.pdf")
+        dvh_file = os.path.join(output_folder, subject_name + "_dvh.pdf")
         plot_dvh_to_pdf(subject_folder, dvh_file)
